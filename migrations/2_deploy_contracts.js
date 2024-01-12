048d2b9b4cd6bf438c1472df34e24f6e7df8e80b
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-01-12 16:54:31 +0100

    Sorted out the deployment script to deploy the smart contracts to my test network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,21 @@
-// SPDX-License-Identifier: MIT
-const CONTRACT= artifacts.require("CONTRACT");
+const StudentRegistration = artifacts.require("StudentRegistration");
+const GradeManagement = artifacts.require("GradeManagement");
+const PointsManagement = artifacts.require("PointsManagement");
+// Include other contracts as needed, like TestManagement
 
 module.exports = function (deployer) {
-  deployer.deploy(CONTRACT);
+  // Deploy the StudentRegistration contract first
+  deployer.deploy(StudentRegistration).then(function() {
+    // After StudentRegistration is deployed, deploy GradeManagement
+    // Pass the address of StudentRegistration to GradeManagement's constructor
+    return deployer.deploy(GradeManagement, StudentRegistration.address);
+  }).then(function() {
+    // Similarly, deploy PointsManagement
+    // You can pass StudentRegistration's address if required
+    return deployer.deploy(PointsManagement, StudentRegistration.address);
+  });
+  // Continue with other contracts in a similar fashion
+  // .then(function() {
+  //   return deployer.deploy(OtherContract, SomeDependency.address);
+  // });
 };
